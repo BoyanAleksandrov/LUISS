496e1b3ca6571bff8846df2f9e011e25c14e1ece
Boyan A. <80381466+BoyanAleksandrov@users.noreply.github.com>
2024-11-06 16:50:01 +0100

    Filter dictionary items

--- Python/list_dic_exercises.py
+++ Python/list_dic_exercises.py
@@ -8,4 +8,15 @@ def update_score(data, student, subject, new_score):
         data.update(new_dict)
         print(data)
 
-update_score(data, "student4", "maths", 90)
+# update_score(data, "student4", "maths", 90)
+
+data2 = {"apple":10, "iphone": 20, "kettle": 5}
+def filter_above_threshold(d, threshold):
+    result = dict()
+    for k,v in d.items():
+          if v > threshold:
+               result.update({k:v})
+    
+    print(result)
+
+filter_above_threshold(data2,5)
