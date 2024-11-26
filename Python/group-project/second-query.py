9afc9171cc2c1ad0e34417c8b8262775817726f1
Boyan A. <80381466+BoyanAleksandrov@users.noreply.github.com>
2024-11-27 00:42:14 +0100

    first version of query

--- Python/group-project/second-query.py
+++ Python/group-project/second-query.py
@@ -1,10 +1,39 @@
 import csv
-movies = {}
-
+genres = {}
+keywords = {}
+result = {}
+genres_names = set()
 with open("movies_table.csv", mode="r") as csvfile:
     reader = csv.DictReader(csvfile)
 
     for row in reader:
-        movies.update({row['id']: None})
+        genres[row['id']] = set()
 
-print(movies)
+with open("genres_table.csv", mode="r") as csvfile:
+    reader = csv.DictReader(csvfile)
+   
+    for row in reader:
+        movie_id = row['id']
+        genre = row['genres']
+        if movie_id in genres:
+            genres[movie_id].add(genre)
+        else:
+            genres[movie_id] = {genre}
+        genres_names.add(genre)
+
+with open("keywords_table.csv", mode="r", encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        movie_id = row['id']
+        keyword = row['keywords']
+ 
+        if movie_id in keywords:
+            keywords[movie_id].add(keyword) 
+        else:
+            keywords[movie_id] = {keyword}
+
+
+
+
+
+print(genres_names)
