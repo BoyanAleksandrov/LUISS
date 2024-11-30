774cbe081e40fcf1ba03afff8546961443a235e7
Boyan A. <80381466+BoyanAleksandrov@users.noreply.github.com>
2024-11-30 03:37:00 +0100

    Final file before pickle file extraction

--- Python/group-project/second-query.py
+++ Python/group-project/second-query.py
@@ -1,39 +1,52 @@
 import csv
-genres = {}
-keywords = {}
-result = {}
-genres_names = set()
-with open("movies_table.csv", mode="r") as csvfile:
+
+result_dict = dict()
+names_genres_dict = dict()
+test_dict = dict()
+
+with open("genres_table.csv", mode="r") as csvfile:
     reader = csv.DictReader(csvfile)
 
     for row in reader:
-        genres[row['id']] = set()
+        id = row['id']
+        result_dict[id] = []
+
+
 
 with open("genres_table.csv", mode="r") as csvfile:
     reader = csv.DictReader(csvfile)
    
     for row in reader:
-        movie_id = row['id']
         genre = row['genres']
-        if movie_id in genres:
-            genres[movie_id].add(genre)
+        id = row['id']
+       
+        if id not in result_dict:
+            result_dict[id] = [genre]
         else:
-            genres[movie_id] = {genre}
-        genres_names.add(genre)
-
-with open("keywords_table.csv", mode="r", encoding="utf-8") as csvfile:
+            result_dict[id].append(genre)
+    
+        
+with open("keywords_table.csv", encoding="utf-8") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        movie_id = row['id']
+        id = row['id']
         keyword = row['keywords']
- 
-        if movie_id in keywords:
-            keywords[movie_id].add(keyword) 
+        if id not in names_genres_dict:
+            names_genres_dict.setdefault(id, 1)
         else:
-            keywords[movie_id] = {keyword}
+            names_genres_dict[id] = names_genres_dict[id] + 1
+
+
+for el in result_dict.values():
+    for val in el:
+        if val not in test_dict:
+            test_dict.setdefault(val, 0)
+for tr in names_genres_dict.keys():
+    if tr in result_dict:
+        for it in result_dict[tr]:
+            if it in test_dict:
+                test_dict[it] = test_dict[it] + names_genres_dict[tr]
 
 
 
-
-
-print(genres_names)
+print(test_dict)
