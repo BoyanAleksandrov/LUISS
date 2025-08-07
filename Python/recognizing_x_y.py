fc7d0548de4b228c9842da1d83b4544ae95706d1
Boyan A. <80381466+BoyanAleksandrov@users.noreply.github.com>
2025-08-07 16:20:56 +0200

    PyTorch data types

--- Python/recognizing_x_y.py
+++ Python/recognizing_x_y.py
@@ -7,8 +7,8 @@ model = tf.keras.Sequential([
 ])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-x = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0], dtype=float)
-y = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0, 199.0, 201.0, 203.0, 205.0, 207.0, 209.0, 211.0, 213.0, 215.0, 217.0, 219.0], dtype=float)
+x = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+y = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
 
 model.fit(x, y, epochs=500)
 
